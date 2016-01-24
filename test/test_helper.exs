c0c2f6a181ec8a8d007301d4416d7a1d45d2acfc
Christoph Grabo <asaaki@mannaz.cc>
2016-01-24 17:42:51 +0100

    Remove obsolete test files

--- test/test_helper.exs
+++
@@ -1,8 +0,0 @@
-ExUnit.configure capture_log: true,
-                 max_cases:   1
-
-Pavlov.start # see: https://github.com/sproutapp/pavlov
-
-# Important note:
-# use Pavlov.Case
-# ^-- do not set `async: true`, otherwise capturing of IO fails miserably
