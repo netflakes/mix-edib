bb69d3772fba71fb1ff6bdc28f2a44d311eab070
Christoph Grabo <asaaki@mannaz.cc>
2016-10-04 19:23:23 +0200

    Remove obsolete configuration

--- config/config.exs
+++ config/config.exs
@@ -1,14 +1,1 @@
 use Mix.Config
-
-if Mix.env == :docs do
-  config :ex_doc, :markdown_processor, ExDoc.Markdown.Cmark
-end
-
-if Mix.env == :lint do
-  config :dogma,
-    rule_set: Dogma.RuleSet.All,
-    exclude: [~r"\A(test|spec)/"],
-    override: %{
-      LineLength => [max_length: 120]
-    }
-end
